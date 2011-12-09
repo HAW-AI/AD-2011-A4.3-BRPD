d73cd4f0c49e5a18ea61a1b1d4ffd719659c987d
Ben <salzig+github@gmail.com>
2011-12-09 16:16:04 +0100

    initial files

---
+++ com/github/haw/brpd/ad/a04/Application.java
@@ -0,0 +1,13 @@
+package com.github.haw.brpd.ad.a04;
+
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com/github/haw/brpd/ad/a04/Programm.java
@@ -0,0 +1,6 @@
+package com.github.haw.brpd.ad.a04;
+public class Programm {
+	static Programm Programme(String source) {
+		return null;
+	}
+}
