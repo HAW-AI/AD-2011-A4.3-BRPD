09c85e85d700a2bd2171008a1030efe7045b2e7e
Ben <salzig+github@gmail.com>
2011-12-09 16:54:37 +0100

    clean up

--- com/github/haw/brpd/ad/a04/Application.java
+++ com/github/haw/brpd/ad/a04/Application.java
@@ -3,17 +3,13 @@ package com.github.haw.brpd.ad.a04;
 import static com.github.haw.brpd.ad.a04.Programm.*;
 
 public class Application {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		String[] input = new String[] {"W+W+WCPC","W--CP+++C"};
-		
 		for (String e : input) {
 			exec(programm(e));
-			System.out.println("---------------------------------------------");
 		}
 	}
-
 }
